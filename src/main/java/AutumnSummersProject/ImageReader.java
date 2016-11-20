b2d4983318791940e95fd248a98c68e65e8dee19
msummer300 <msummer@tulane.edu>
2016-11-20 12:40:14 -0600

    Updated image reader

--- src/main/java/AutumnSummersProject/ImageReader.java
+++ src/main/java/AutumnSummersProject/ImageReader.java
@@ -2,24 +2,30 @@ package AutumnSummersProject;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
 import java.io.File;
 
 /**
  * Created by Autumn on 10/4/2016.
  */
 public class ImageReader {
-    public BufferedImage image;
+    public String image;
 
     //interprets an image based on the string representation of it's location
     public ImageReader(String location) {
         try {
-            //System.out.print(location);
-            //location = location.substring(5);
+            DataBuffer buffer;
+            String current;
+            image = "";
             String loc = "data/AutumnSummersData/" + location;
-            //System.out.print(loc);
             File inputFile = new File(loc);
-            image = ImageIO.read(inputFile)
-            //image = ImageIO.read(inputFile).getData().getDataBuffer().getOffset();
+            buffer = ImageIO.read(inputFile).getData().getDataBuffer();
+            for(int i=0; i < (buffer.getSize() - 1); i++){
+                current = String.valueOf(buffer.getElem(i));
+                image.concat(current);
+                image.concat(" ");
+            }
+            //System.out.println("Processed image!");
         }
         catch (Exception e){}
     }
